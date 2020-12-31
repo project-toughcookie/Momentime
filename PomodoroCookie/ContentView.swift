0c62ec38606550f62d30fb5041881e41beefe9ea
Junyoung, Sung <junyoung.sung@navercorp.com>
2020-12-31 15:01:47 +0900

    Initial Commit

---
+++ PomodoroCookie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PomodoroCookie
+//
+//  Created by 성준영 on 2020/12/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
