f9cfd980263aac5481ce16ec8b9cd65e84312956
sungjunyoung <sungjunyoung@yereol-ui-MacBook-Pro.local>
2021-02-01 22:38:36 +0900

    POMO-53 카탈리나 대응 레티나 디스플레이

--- Momentime/Managers/Calendar/EventStore.swift
+++ Momentime/Managers/Calendar/EventStore.swift
@@ -17,11 +17,11 @@ extension EKEventStore: EventStore {
     func eventAuthorizationStatus() -> EKAuthorizationStatus {
         EKEventStore.authorizationStatus(for: .event)
     }
-
+    
     func requestAccess(handler: @escaping (Bool, Error?) -> Void) {
         EKEventStore().requestAccess(to: EKEntityType.event, completion: handler)
     }
-
+    
     func getCalendars() -> [TaskCalendar] {
         let ekCalendars = EKEventStore().calendars(for: .event)
         return ekCalendars.map { (ekCalendar: EKCalendar) -> TaskCalendar in
@@ -34,10 +34,10 @@ extension EKEventStore: EventStore {
             )
         }
     }
-
+    
     func getTodayTasks(calendarId: String) throws -> [Task] {
         let store = EKEventStore()
-
+        
         let current = Calendar.current
         let now = Date()
         let start: Date
@@ -52,10 +52,11 @@ extension EKEventStore: EventStore {
         } catch {
             throw error
         }
-
+        
+        
         let calendar = store.calendar(withIdentifier: calendarId)
         let predicate = store.predicateForEvents(withStart: start, end: end, calendars: [calendar!])
-
+        
         let ekEvents = store.events(matching: predicate)
         return ekEvents.map { (ekEvent: EKEvent) -> Task in
             Task(
@@ -68,21 +69,21 @@ extension EKEventStore: EventStore {
             )
         }
     }
-
+    
     func toggleTaskDone(taskId: String) throws {
         let store = EKEventStore()
-
+        
         if let event = store.event(withIdentifier: taskId) {
             if event.notes == nil {
                 event.notes = ""
             }
-
+            
             if event.notes!.contains("[Done] ") {
                 event.notes = event.notes?.replacingOccurrences(of: "[Done] ", with: "")
             } else {
                 event.notes = "[Done] " + event.notes!
             }
-
+            
             do {
                 try store.save(event, span: EKSpan.thisEvent)
             } catch {
@@ -90,7 +91,7 @@ extension EKEventStore: EventStore {
             }
             return
         }
-
+        
         throw CookieError.TaskUpdateFailed
     }
 }
